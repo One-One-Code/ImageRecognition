72b1a7c14ba40a28160491cf67177edb13f30a10
Coding-Huang <44698989+One-One-Code@users.noreply.github.com>
2019-04-14 15:20:32 +0800

    Add files via upload

---
+++ ImageHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvTest
+{
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Imaging;
+    using System.Runtime.InteropServices;
+
+    public static class ImageHelper
+    {
+        /// <summary>
+        /// 图片等比例缩放
+        /// </summary>
+        /// <param name="imgToResize"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static System.Drawing.Image ResizeImage(this System.Drawing.Image imgToResize, Size size)
+        {
+            //获取图片宽度
+            int sourceWidth = imgToResize.Width;
+            //获取图片高度
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //计算宽度的缩放比例
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //计算高度的缩放比例
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //期望的宽度
+            int destWidth = (int)(sourceWidth * nPercent);
+            //期望的高度
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //绘制图像
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+
+        public static Bitmap CutImage(Bitmap rawImage, Rectangle rectangle)
+        {
+            var image = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format24bppRgb);
+            System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(image);
+
+            graphics.DrawImage(rawImage, new Rectangle(0, 0, image.Width, image.Height), rectangle, System.Drawing.GraphicsUnit.Pixel);
+            return image;
+        }
+
+        public static Bitmap CutImageByBit(Bitmap rawImage, Rectangle rectangle)
+        {
+            var image = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format24bppRgb);
+            //var bmpData = rawImage.LockBits(rectangle, ImageLockMode.ReadOnly, rawImage.PixelFormat);
+            //var bmpDataNew = image.LockBits(new Rectangle(0,0, image.Width, image.Height), ImageLockMode.WriteOnly, image.PixelFormat);
+            //for (int i = rectangle.X; i < rectangle.X + rectangle.Width; i++)
+            //{
+            //    for (int j = rectangle.Y; j < rectangle.Y + rectangle.Height; j++)
+            //    {
+            //        byte b = Marshal.ReadByte(bmpData.Scan0 + (i * 3 + bmpData.Stride * j));
+            //        byte g = Marshal.ReadByte(bmpData.Scan0 + (i * 3 + bmpData.Stride * j) + 1);
+            //        byte r = Marshal.ReadByte(bmpData.Scan0 + (i * 3 + bmpData.Stride * j) + 2);
+            //        Marshal.WriteByte(bmpDataNew.Scan0 + ((i- rectangle.X) * 3 + bmpDataNew.Stride * (j- rectangle.Y)),b);
+            //        Marshal.WriteByte(bmpDataNew.Scan0 + ((i - rectangle.X) * 3 + bmpDataNew.Stride * (j - rectangle.Y)+1), g);
+            //        Marshal.WriteByte(bmpDataNew.Scan0 + ((i - rectangle.X) * 3 + bmpDataNew.Stride * (j - rectangle.Y)+2), r);
+            //    }
+              
+            //}
+            //rawImage.UnlockBits(bmpData);
+            //image.UnlockBits(bmpDataNew);
+            return image;
+        }
+    }
+}
---
+++ QrCodeHelper.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvTest
+{
+    using System.Drawing;
+    using System.IO;
+
+    using Emgu.CV;
+    using Emgu.CV.CvEnum;
+    using Emgu.CV.Structure;
+    using Emgu.CV.Util;
+
+    using ZXing;
+
+    public class QrCodeHelper
+    {
+        /// <summary>
+        /// 从Qr中获取内容
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public string GetQrData(string path)
+        {
+            Image<Gray, Byte> image = new Image<Gray, Byte>(path),
+                   dist1 = image.CopyBlank(),
+                   dest2 = image.CopyBlank();
+
+            Image<Gray, Byte> threshimg = new Image<Gray, Byte>(image.Width, image.Height);
+            CvInvoke.Threshold(image, threshimg, 120, 255, ThresholdType.Binary);
+            //threshimg.Save(string.Format(@"E:\data\image\testimage\{0}-threshimg.jpg", Path.GetFileNameWithoutExtension(path)));
+            var contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(threshimg, contours, dist1, RetrType.List, ChainApproxMethod.ChainApproxNone);
+            var results = new List<Rectangle>();
+            for (var k = 0; k < contours.Size; k++)
+            {
+                if (contours[k].Size < 50)
+                {
+                    continue;
+                }
+
+                var rectangle = CvInvoke.BoundingRectangle(contours[k]);
+
+                float w = rectangle.Width;
+                float h = rectangle.Height;
+                float rate = Math.Min(w, h) / Math.Max(w, h);
+                if (rate > 0.85)
+                {
+                    dest2.Draw(rectangle, new Gray(255));
+                    results.Add(rectangle);
+                }
+
+            }
+            dest2.Save(string.Format(@"E:\data\image\testimage\{0}-range.jpg", Path.GetFileNameWithoutExtension(path)));
+            //按照二维码规则，过滤不符合规范的区域
+            var filterResults = new List<Rectangle>();
+            foreach (var rectangle in results)
+            {
+                //if (results.Exists(
+                //    p => p.X > rectangle.X && p.X + p.Width < rectangle.X + rectangle.Width && p.Y > rectangle.Y && p.Y + p.Height < rectangle.Y + rectangle.Height))
+                //{
+                //    filterResults.Add(rectangle);
+                //}
+                filterResults.Add(rectangle);
+            }
+            //根据矩形面积进行分组，面积允许误差范围为10%
+            var sizeGroups = new Dictionary<int, List<Rectangle>>();
+            foreach (var filterResult in filterResults)
+            {
+                var size = filterResult.Height * filterResult.Width;
+                foreach (var sizeGroup in sizeGroups)
+                {
+                    if (Math.Abs(sizeGroup.Key - size) <= sizeGroup.Key * 0.1)
+                    {
+                        sizeGroup.Value.Add(filterResult);
+                    }
+                }
+                if (!sizeGroups.ContainsKey(size))
+                {
+                    var items = new List<Rectangle>() { filterResult };
+                    sizeGroups[size] = items;
+                }
+            }
+            //遍历分组，找出最符合二维码的组合
+            var groupItems = new List<List<Rectangle>>();
+            foreach (var sizeGroup in sizeGroups)
+            {
+                if (sizeGroup.Value.Count < 3)
+                {
+                    continue;
+                }
+
+                var combinationResults = GetCombinationList(sizeGroup.Value.Count, 3);
+                foreach (var combinationResult in combinationResults)
+                {
+                    var r1 = sizeGroup.Value[combinationResult[0]];
+                    var r2 = sizeGroup.Value[combinationResult[1]];
+                    var r3 = sizeGroup.Value[combinationResult[2]];
+                    if (Math.Abs(r1.Width - r2.Width) < 3 && Math.Abs(r1.Height - r2.Height) < 3
+                                                          && Math.Abs(r2.Width - r3.Width) < 3
+                                                          && Math.Abs(r2.Height - r3.Height) < 3)
+                    {
+                        int x = 0;
+                        int y = 0;
+                        if (Math.Abs(r1.X - r2.X) < r1.Width * 0.25)
+                        {
+                            y = Math.Abs(r1.Y - r2.Y);
+                            var min = r1.Y > r2.Y ? r2 : r1;
+                            if (Math.Abs(min.Y - r3.Y) > r1.Height * 0.25)
+                            {
+                                continue;
+                            }
+                            x = Math.Abs(r1.X - r3.X);
+                        }
+                        else if (Math.Abs(r1.Y - r2.Y) < r1.Height * 0.25)
+                        {
+                            y = Math.Abs(r1.Y - r3.Y);
+                            var min = r1.X > r2.X ? r2 : r1;
+                            if (Math.Abs(min.X - r3.X) > r1.Width * 0.25)
+                            {
+                                continue;
+                            }
+                            x = Math.Abs(r1.X - r2.X);
+                        }
+                        else if (Math.Abs(r1.Y - r3.Y) < r1.Height * 0.25)
+                        {
+                            y = Math.Abs(r1.Y - r2.Y);
+                            var min = r1.X > r3.X ? r3 : r1;
+                            if (Math.Abs(min.X - r2.X) > r1.Width * 0.25)
+                            {
+                                continue;
+                            }
+                            x = Math.Abs(r1.X - r3.X);
+                        }
+                        else if (Math.Abs(r1.X - r3.X) < r1.Width * 0.25)
+                        {
+                            y = Math.Abs(r1.Y - r3.Y);
+                            var min = r1.Y > r3.Y ? r3 : r1;
+                            if (Math.Abs(min.Y - r2.Y) > r1.Height * 0.25)
+                            {
+                                continue;
+                            }
+                            x = Math.Abs(r1.X - r2.X);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        if (Math.Abs(x - y) < r1.Width * 0.25)
+                        {
+                            var item = new List<Rectangle>();
+                            item.Add(r1);
+                            item.Add(r2);
+                            item.Add(r3);
+                            groupItems.Add(item);
+                        }
+                    }
+                }
+            }
+
+            if (groupItems.Count == 0)
+            {
+                return null;
+            }
+            var max = groupItems.OrderByDescending(p => (p[0].Height * p[0].Width)).ElementAt(0);
+            //var newRects = max.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+            //if (newRects[0].X != newRects[1].X && newRects[0].Y != newRects[2].Y)
+            //{
+            //   //三点仿射进行图片纠正
+            //    var srcPoints = new PointF[]
+            //                    {
+            //                        new PointF(newRects[0].X, newRects[0].Y), new PointF(newRects[1].X, newRects[1].Y),
+            //                        new PointF(newRects[2].X, newRects[2].Y)
+            //                    };
+            //    var destPoints = new PointF[]
+            //                     {
+            //                         new PointF(newRects[0].X, newRects[0].Y), new PointF(newRects[0].X, newRects[1].Y),
+            //                         new PointF(newRects[2].X, newRects[0].Y)
+            //                     };
+            //    var warpImage = RotateHelper.WarpPerspective(image.Bitmap, srcPoints, destPoints);
+            //    image = new Image<Gray, Byte>(warpImage);
+            //    image.Save(string.Format(@"E:\data\image\testimage\{0}-Rotate.jpg", Path.GetFileNameWithoutExtension(path)));
+            //}
+
+            var minx = max.Min(p => p.X) - 10;
+            var miny = max.Min(p => p.Y) - 10;
+            var width = max.Max(p => (p.X + p.Width)) + 20;
+            var height = max.Max(p => (p.Y + p.Height)) + 20;
+            var code = ImageHelper.CutImage(image.Bitmap, new Rectangle(minx, miny, width - minx, height - miny));
+            code.Save(string.Format(@"E:\data\image\testimage\{0}-qr.jpg", Path.GetFileNameWithoutExtension(path)));
+            BarcodeReader reader = new BarcodeReader();
+            reader.Options.CharacterSet = "utf-8";
+            reader.Options.TryHarder = true;
+            var decodeObj = reader.Decode(code);
+            if (decodeObj != null)
+            {
+                return decodeObj.Text;
+
+            }
+            else
+            {
+                int count = 0;
+                while (decodeObj == null)
+                {
+                    if (count > 2)
+                    {
+                        break;
+                    }
+                    code = (Bitmap)code.ResizeImage(
+                        new Size((int)Math.Round(code.Width * 0.8), (int)Math.Round(code.Height * 0.8)));
+                    decodeObj = reader.Decode(code);
+                    if (decodeObj != null)
+                    {
+                        return decodeObj.Text;
+                    }
+
+                    count++;
+                }
+
+            }
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// 获取排列组合的结果集
+        /// </summary>
+        /// <param name="count">总共多少元素</param>
+        /// <param name="perCount">每组多少个</param>
+        /// <returns></returns>
+        private List<List<int>> GetCombinationList(int count, int perCount)
+        {
+            var result = new List<List<int>>();
+            for (int i = 0; i < count - perCount + 1; i++)
+            {
+                for (int j = i + 1; j < count - perCount + 2; j++)
+                {
+                    for (int k = j + 1; k < count; k++)
+                    {
+                        var item = new List<int>();
+                        item.Add(i);
+                        item.Add(j);
+                        item.Add(k);
+                        result.Add(item);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
